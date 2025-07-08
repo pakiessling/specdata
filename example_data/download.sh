d337cd7fedbd4b902777d96a7798ced3e7bda385
Paul Kiessling <pakiessling@ukaachen.de>
2025-07-08 15:46:39 +0200

    added example data download script

---
+++ example_data/download.sh
@@ -0,0 +1,4 @@
+wget -P data ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2014/11/PXD001283/HR2MSImouseurinarybladderS096-opticalimage.tif
+wget -P data ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2014/11/PXD001283/HR2MSImouseurinarybladderS096-results.csv
+wget -P data ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2014/11/PXD001283/HR2MSImouseurinarybladderS096.ibd
+wget -P data ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2014/11/PXD001283/HR2MSImouseurinarybladderS096.imzML
